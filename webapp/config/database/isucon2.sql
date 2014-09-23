8a413d6a62b9d5568c8373f82eb30a90d8382c00
cnosuke <shinnosuke@gmail.com>
2014-09-23 18:53:09 +0900

    InnoDB to MEMORY

--- webapp/config/database/isucon2.sql
+++ webapp/config/database/isucon2.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS isucon2.artist (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+) ENGINE=MEMORY;
 
 CREATE TABLE IF NOT EXISTS isucon2.ticket (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -19,14 +19,14 @@ CREATE TABLE IF NOT EXISTS isucon2.ticket (
   `name` VARCHAR(255) NOT NULL,
   `artist_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+) ENGINE=MEMORY;
 
 CREATE TABLE IF NOT EXISTS isucon2.variation (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   `ticket_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+) ENGINE=MEMORY;
 
 CREATE TABLE IF NOT EXISTS isucon2.stock (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -37,14 +37,14 @@ CREATE TABLE IF NOT EXISTS isucon2.stock (
   PRIMARY KEY (`id`),
   UNIQUE KEY `variation_seat` (`variation_id`,`seat_id`),
   KEY `idx` (`order_id`,`seat_id`,`variation_id`)
-) ENGINE=InnoDB;
+) ENGINE=MEMORY;
 
 CREATE TABLE IF NOT EXISTS isucon2.order_request (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `member_id` VARCHAR(32) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `member_order` (`member_id`)
-) ENGINE=InnoDB;
+) ENGINE=MEMORY;
 
 CREATE TABLE IF NOT EXISTS isucon2.recent_sold (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -55,4 +55,4 @@ CREATE TABLE IF NOT EXISTS isucon2.recent_sold (
   `v_name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `order_id` (`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=MEMORY DEFAULT CHARSET=utf8;
